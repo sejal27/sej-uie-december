9f8342ceb7025515a8e931e5985e8b3f480a26f7
Sejal Parikh <sejalrparikh@gmail.com>
2023-12-02 01:28:24 -0500

    phew, calendar view ready

--- src/app/extensions/dec-2.jsx
+++ src/app/extensions/dec-2.jsx
@@ -1,15 +1,79 @@
+`/* eslint-disable react/prop-types */
 import React from "react";
-import { hubspot, Text } from "@hubspot/ui-extensions";
+import {
+  eachWeekOfInterval,
+  startOfMonth,
+  endOfMonth,
+  differenceInDays,
+  addYears,
+} from "date-fns";
+import {
+  hubspot,
+  Text,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  Heading,
+} from "@hubspot/ui-extensions";
 
-// Define the extension to be run within the Hubspot CRM
-hubspot.extend(({ runServerlessFunction }) => (
-  <December2 runServerless={runServerlessFunction} />
-));
+hubspot.extend(({ context }) => <CalendarView context={context} />);
+const CalendarView = ({ context }) => {
+  let today = new Date();
+  const weeks = eachWeekOfInterval({
+    start: startOfMonth(today),
+    end: endOfMonth(today),
+  });
+  console.log("today:", today);
 
-const December2 = () => {
+  const christmasThisYear = new Date(today.getFullYear(), 11, 25);
+  const daysUntilchristmas = differenceInDays(christmasThisYear, today);
+
   return (
     <>
-      <Text>Hello World</Text>
+      <Text>
+        Hello {context.user.firstName}! You have {daysUntilchristmas} remaining until Christmas
+        🎄🎄🎄.
+      </Text>
+
+      <Table bordered={true}>
+        <TableHead>
+          <TableRow>
+            {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day) => (
+              <TableHeader align="center" key={day}>
+                {day}
+              </TableHeader>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {weeks.map((weekStart, i) => (
+            <TableRow key={i}>
+              {Array.from({ length: 7 }).map((_, j) => {
+                const day = new Date(weekStart);
+                day.setDate(day.getDate() + j);
+                if (day.getMonth() !== today.getMonth()) {
+                  return <TableCell key={j} />;
+                }
+                return (
+                  <TableCell align="center" key={j}>
+                    {day.getDate() === today.getDate() ? (
+                      <Heading>🏆 {day.getDate()} 🏆</Heading>
+                    ) : day.getDate() === 25 && day.getMonth() === 11 ? (
+                      <Heading>🎄 {day.getDate()} 🎄</Heading>
+                    ) : (
+                      <Heading>{day.getDate()}</Heading>
+                    )}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </>
   );
 };
+`;
