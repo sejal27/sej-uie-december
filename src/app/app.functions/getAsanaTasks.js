224977b59a7d740a7b25d4d47bb3b718d9dc1f73
Sejal Parikh <sejalrparikh@gmail.com>
2023-12-07 01:44:08 -0500

    get asana tasks

--- src/app/app.functions/getAsanaTasks.js
+++ src/app/app.functions/getAsanaTasks.js
@@ -1,28 +1,40 @@
 const axios = require("axios");
-const asana = require("asana");
 
-// Make sure you have added these as secrets in your account using `hs secrets add`
-const { ASANA_PAT, ASANA_WS_GID } = process.env;
-const asanaClient = asana.Client.create().useAccessToken(ASANA_PAT);
+const { ASANA_PAT } = process.env;
 
 exports.main = async (context = {}) => {
-  const { hs_object_id } = context.propertiesToSend;
-  const asanaTaskTag = "hs_" + hs_object_id;
+  const { ASANA_PROJECT_GID } = context.parameters;
+  console.log("IDs", ASANA_PROJECT_GID);
+
+  console.log("inside serverless function getAsanaTasks");
+
+  let opts = { limit: 10, opt_fields: "gid,name,assignee,completed,due_on" };
 
   try {
-    const tasks = await getTasks(asanaTaskTag);
+    const resp = await axios.get(
+      `https://app.asana.com/api/1.0/projects/${ASANA_PROJECT_GID}/tasks?opt_fields=name,assignee,completed,due_on`,
+      {
+        headers: {
+          Authorization: `Bearer ${ASANA_PAT}`,
+        },
+      }
+    );
+    console.log("tasks", resp);
+
+    const users = await asanaClient.users.getUsersForTeam(ASANA_TEAM_GID);
+    console.log("users", users);
+    const tasks = resp.data.data.map((task) => {
+      return {
+        name: task.name,
+        assignee: task.assignee,
+        completed: task.completed,
+        due_on: task.due_on,
+      };
+    });
+    console.log("tasks", tasks);
     return tasks;
   } catch (error) {
+    console.log("error", error);
     return error;
   }
 };
-
-// ================== vvvv  Helper functions  vvvv =====================
-
-// Get Asana tasks for the workspace and the team. Make sure you created a global custom field, added to the project, as well as added ASANA_CUSTOM_FIELD in your secrets and
-async function getTasks(hs_contact_id) {
-  const tasks = asanaClient.tasks.searchTasksForWorkspace(ASANA_WS_GID, {
-    opt_fields: "name,permalink_url,due_on,notes,assignee.name,completed",
-  });
-  return tasks;
-}
--- src/app/extensions/AsanaIntegration.jsx
+++ src/app/extensions/AsanaIntegration.jsx
@@ -1,16 +1,45 @@
-import React from "react";
-import { hubspot } from "@hubspot/ui-extensions";
+import React, { useState } from "react";
+import { hubspot, Button, Flex, Box } from "@hubspot/ui-extensions";
 import Task from "./components/Task.jsx";
 
 const ASANA_WS_GID = "8587152060687";
 const ASANA_TEAM_GID = "1202716730818736";
+const ASANA_PROJECT_GID = "1206117893165586";
 
-hubspot.extend(() => <Extension />);
+const Asana = ({ runServerlss, fetchProperties, context }) => {
+  const [AsanaTasks, setAsanaTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const Extension = () => {
+  const getAsanaTasks = () => {
+    runServerlss({
+      name: "getAsanaTasks",
+      parameters: {
+        // ASANA_WS_GID: ASANA_WS_GID,
+        // ASANA_TEAM_GID: ASANA_TEAM_GID,
+        ASANA_PROJECT_GID: ASANA_PROJECT_GID,
+      },
+    })
+      .then((resp) => {
+        setAsanaTasks(resp);
+      })
+      .finally(() => setLoading(false));
+  };
   return (
-    <>
-      {/* <Task name="checkbox" initialIsChecked={false} taskValue="Task 1 text" /> */}
-    </>
+    <Flex direction="column" gap="md">
+      <Task name="checkbox" initialIsChecked={false} taskValue="Task 1 text" />
+      <Box>
+        <Button size="sm" onClick={getAsanaTasks}>
+          Refresh
+        </Button>
+      </Box>
+    </Flex>
   );
 };
+
+hubspot.extend(({ runServerlessFunction, actions, context }) => (
+  <Asana
+    runServerlss={runServerlessFunction}
+    fetchProperties={actions.fetchCrmObjectProperties}
+    context={context}
+  />
+));
